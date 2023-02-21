a80b74ddc8c79601a8b94d704a5e4b864c7a0a03
Aaron Stannard <aaron@petabridge.com>
2023-02-21 11:40:03 -0600

    added complete tests

--- src/DData.Counters.Tests/DDataCounterSpecs.cs
+++ src/DData.Counters.Tests/DDataCounterSpecs.cs
@@ -1,10 +1,12 @@
 using Akka.Actor;
 using Akka.Cluster;
 using Akka.Cluster.Hosting;
+using Akka.DistributedData;
 using Akka.Hosting;
 using Akka.Hosting.TestKit;
 using Akka.Remote.Hosting;
 using DData.Counters.Actors;
+using FluentAssertions;
 using Xunit.Abstractions;
 using static DData.Counters.Actors.DDataKeys;
 
@@ -22,7 +24,7 @@ public class DDataCounterSpecs : TestKit
         {
             return Cluster.Get(Sys).State.Members.All(m => m.Status == MemberStatus.Up);
         });
-        var selfAddress = Cluster.Get(Sys).SelfAddress;
+        var selfAddress = Cluster.Get(Sys).SelfUniqueAddress;
         
         var replicator = await ActorRegistry.GetAsync<ReplicatorKey>();
         var reader = Props.Create(() => new DDataCounterReader(replicator));
@@ -31,9 +33,19 @@ public class DDataCounterSpecs : TestKit
         var initialState = readerActorRef.UnderlyingActor.GCounter;
 
         // act
-        
-        
+        foreach (var i in Enumerable.Range(0, 2))
+        {
+            var update1 = Dsl.Update(CounterKey, GCounter.Empty, WriteLocal.Instance,
+                g => g.Increment(selfAddress));
+            await replicator.Ask<UpdateSuccess>(update1);
+        }
+
         // assert
+        await AwaitAssertAsync(() =>
+        {
+            readerActorRef.UnderlyingActor.GCounter.State.TryGetValue(selfAddress, out var value);
+            value.Should().Be(2);
+        });
     }
 
     protected override void ConfigureAkka(AkkaConfigurationBuilder builder, IServiceProvider provider)
--- src/DData.Counters/Actors/DDataCounterReader.cs
+++ src/DData.Counters/Actors/DDataCounterReader.cs
@@ -12,7 +12,7 @@ public sealed class DDataCounterReader : ReceiveActor
     private readonly ILoggingAdapter _logger = Context.GetLogger();
     private readonly IActorRef _replicator;
     
-    public readonly GCounter GCounter = GCounter.Empty;
+    public GCounter GCounter = GCounter.Empty;
 
     public DDataCounterReader(IActorRef replicator)
     {
@@ -22,13 +22,15 @@ public sealed class DDataCounterReader : ReceiveActor
         Receive<Changed>(changed => changed.Key.Equals(CounterKey), update =>
         {
             var counter = update.Get(CounterKey);
-            GCounter.Merge(counter);
+           GCounter = GCounter.Merge(counter);
             _logger.Info("Counter changed to {0}", counter);
             // print counter values from all nodes
             foreach (var i in counter.State)
             {
                 _logger.Info($"Counter value on node {i.Key} is {i.Value}");
             }
+            
+            _logger.Info($"Aggregate counter value is {GCounter.Value}");
         });
     }
 
